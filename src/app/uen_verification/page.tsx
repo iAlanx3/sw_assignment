9b211f107897f92637f2fa3b7f3b4704226c4d89
Alan <alan_seng97@hotmail.com>
2025-05-10 01:24:47 +0800

    styling for uen validator done

--- src/app/uen_verification/page.tsx
+++ src/app/uen_verification/page.tsx
@@ -12,12 +12,14 @@ interface hintBox {
 export default function UENVerification_Page() {
 	const [hint, setHint] = useState<hintBox[]>([]);
 	return (
-		<div className="flex flex-col bg-gray-100 min-h-screen items-center justify-center px-8 pb-20 sm:px-20 font-[family-name:var(--font-geist-sans)]">
-			<main className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-8 items-center z-10">
-				<h1 className="mx-auto">UEN Validator</h1>
+		<div className="flex flex-col bg-gray-100 min-h-[101vh] items-center justify-start px-8 pb-20 sm:px-20 font-[family-name:var(--font-geist-sans)]">
+			<main className="flex flex-col gap-8 items-center w-full max-w-md mt-[33vh]">
+				<h1 className="mx-auto font-bold text-2xl text-center">
+					UEN Validator
+				</h1>
 				<InputField setHint={setHint} />
 			</main>
-			<div className="flex flex-col w-full items-center mt-[60vh] gap-y-4">
+			<div className="flex flex-col items-center w-full mt-5 gap-y-4 px-4">
 				{hint.map((h, index) => (
 					<HintCard
 						className="hint-card w-full max-w-md"
